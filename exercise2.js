1618c1a690c2fe2dd9ed2c443d18e2a0261f7098
Nancy Golden <pngolden@verizon.net>
2020-05-27 14:17:59 -0500

    making progress

--- exercise2.js
+++ exercise2.js
@@ -3,10 +3,10 @@
 /************************************************************* */
 // Problem 1: Refactor the for() loop to be a while loop.
 //
-
+/*
 for(let i=0; i<10; i++) {
     console.log(" the value of i in the loop is : " + i);
-}
+}*/
 
 
 //your code...
@@ -16,10 +16,26 @@ for(let i=0; i<10; i++) {
 
 /************************************************************* */
 // Problem 2:
-// multiply the sum of 30 added to two by 20.  Divide the product by 10 raised to the power of 2
-//use javascript to compute the value of the above statement. Each individual operation needs to be a function expression. run all the functions after defining them, and print the answer to the console.
+// multiply the sum of 30 added to two by 20.  Divide the product by 10 raised to 
+//the power of 2 use javascript to compute the value of the above statement. Each 
+//individual operation needs to be a function expression. run all the functions 
+//after defining them, and print the answer to the console.
 
 //your code...
+
+/*
+let add = function ( num1, num2 ) {
+    var separated=wizards.split(' ');
+    console.log(separated)
+    var join=separated.join(' ');
+    console.log(join)   
+
+}
+
+converttoarray("The five boxing wizards jump quickly.");
+
+*/
+
 
 
 /************************************************************* */
@@ -45,7 +61,7 @@ for(let i=0; i<10; i++) {
 /************************************************************* */
 // Problem 4:
 // Refactor the following code using a switch statement:
-
+/*
 const day = "friday";
 
 if(day === "monday") {
@@ -62,10 +78,12 @@ if(day === "monday") {
     console.log("It's a weekend!")
 }
 
-
+*/
 
 /************************************************************* */
 // Problem 5: Refactor the following functions to use a ternary expression:
+
+/*
 const age = 10;
 if (age > 21) console.log("adult"); else {
     console.log("minor");
@@ -78,10 +96,11 @@ if (age > 13 && age < 19) console.log('teen'); else {
 if (age > 65) console.log("retired"); else {
     console.log("still working...");
 }
-
+*/
 
 /************************************************************* */
-// Problem 6: Create an object literal that represents yourself.  set it to a variable that appropriately describes the object.  Include the following properties:
+// Problem 6: Create an object literal that represents yourself.  set it to a variable that
+//appropriately describes the object.  Include the following properties:
 /*
 -name
 -age
@@ -90,18 +109,42 @@ if (age > 65) console.log("retired"); else {
 -profession
 -education
 
--add a method on your object, named learn. the learn method should print the value of the name property using the 'this' keyword.
+-add a method on your object, named learn. the learn method should print the value 
+//of the name property using the 'this' keyword.
 
 -add another method on your object of any action you want to perform, using some property that exists on your object, making sure to utilize the 'this' keyword.
 */
 
 //your code...
-
-
+//nancy is an object. The left column is property names. The right column are values.
+//Nancy comments: functions attached to objects are called methods.
+//Definition for this keyword: The JavaScript this keyword refers to the object it 
+//belongs to. It has different values depending on where it is used: In a method, 
+//this refers to the owner object. Alone, this refers to the global object.
+let student = { //student is the owner of the method
+    firstName: 'Nancy',
+    lastName: 'Golden',
+    age: 'young at heart',
+    gender: 'female',
+    hobbies: ['bicycling', 'horsebackriding', 'table tennis'],
+    profession: 'teacher',
+    education: 'masters degree',
+    learn: function(){ //function expression (method)
+        return this.firstName + " " + this.lastName; //this means student
+        
+//learnmore: function(){ //function expression (method)
+//return this.hobbies; //this means student >> Why doesn't this work? How do you 
+//add additional methods?
+    
+    }
+};
+console.log(student.learn());
+console.log(student.morelearn());
+//console.log(this.firstName + " " + this.lastName);
 /************************************************************* */
 // Problem 6: Create an object literal that represents any object in the real world that you like.  Try to come up with a realistic set of properties and methods that would accurately describe that object.  In at least one of your methods, use the this keyword to refer back to one of the properties you defined on this object literal.  
 
-//your code...
+//your code...  
 
 
 
