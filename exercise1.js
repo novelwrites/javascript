ddf6da38a39ef1d20412f068a7cf3b57b87450d4
Nancy Golden <pngolden@verizon.net>
2020-05-26 23:01:28 -0500

    got a lot more work done

--- exercise1.js
+++ exercise1.js
@@ -6,6 +6,7 @@ from Fahrenheit to Celsius. Declare a variable for the current temperature then
 store the Celsius temperature into a variable. Console log the variable.*/
 
 //declare variable for current temperature in fahrenheit - user defines value
+/*
 var currentTempf = prompt('What is the current temp (F)?');
 console.log(currentTempf);
 
@@ -16,12 +17,14 @@ console.log("conversion function")
 }
 
 conversionFC(currentTempf)
+*/
 
 // Part 2: Write another funciton that converts the Celsius temperature back to Fahrenheit. 
 //your code...
 
 //I wanted to use initial celsius value obtained in Problem 1 but it comes up 
 //undefined - why?
+/*
 var celsius = 45;
 console.log(celsius);
 let conversionCF = function(celsius) {
@@ -31,6 +34,8 @@ console.log("conversion function")
 }
 
 conversionCF(celsius)
+
+*/
 
 
 
@@ -44,7 +49,7 @@ conversionCF(celsius)
 
 //your code...
 
-var age = prompt('What is your age?');
+/*var age = prompt('What is your age?');
 console.log(age);
 
 let oldenough = function(age) {
@@ -58,40 +63,57 @@ let oldenough = function(age) {
 }
 oldenough(age)
 
-
-
-
+*/
 
 /************************************************************* */
 // Problem 3:
 // Write a JavaScript function that converts a string to an array. Declare a string variable
 //with "The five boxing wizards jump quickly." Use the split() method to turn the 
-//string into an array of strings. (Be sure you separate the string into words, not characters.) After you have finished, use the join() method to change the array back into a string.
+//string into an array of strings. (Be sure you separate the string into words, not characters.) 
+//After you have finished, use the join() method to change the array back into a string.
 
 //your code...
 
 
+//code below creates the function. Stuff in parenthesis after function are 
+//parameters and stuff in-between curly braces executes when function is called.
+let converttoarray = function (wizards) {
+    var separated=wizards.split(' ');
+    console.log(separated)
+    var join=separated.join(' ');
+    console.log(join)
+    
 
+}
 
+converttoarray("The five boxing wizards jump quickly."); //calls the function; values in () are arguements 
+//passed to the parameters.
 
 
 
 /************************************************************* */
 // Problem 4:
-// Write a JavaScript function with a function that reverses your telephone number. Use the split() and join() methods from the previous problem as well as the toString() method to convert a number into a string and reverse() method to reverse an array in place.
+// Write a JavaScript function with a function that reverses your telephone number. 
+//Use the split() and join() methods from the previous problem as well as the toString() 
+//method to convert a number into a string and reverse() method to reverse an array in place.
 
 //your code...
 
+let telephone = function (phoneNum) {
+    var phoneArray=phoneNum.split('');
+    var phoneReverse=phoneArray.reverse();
+    console.log (phoneReverse)
+}
 
-
-
+telephone("9727866430")
 
 /************************************************************* */
 // Problem 5:
-// Write a JavaScript function that creates a car object using information about your car. Include the make, model, year, and color. Write a function to get the year, color, make, and model in that order.
+// Write a JavaScript function that creates a car object using information about your car. 
+//Include the make, model, year, and color. Write a function to get the year, color, make, 
+//and model in that order.
 
 //your code...
-
 
 
 
@@ -99,17 +121,30 @@ oldenough(age)
 
 /************************************************************* */
 // Problem 6:
-// Write a JavaScript function with a loop that will iterate from 0 to 15. Each iteration, the for loop will check if the current number is odd or even and display the output.
+// Write a JavaScript function with a loop that will iterate from 0 to 15. Each iteration, 
+//the for loop will check if the current number is odd or even and display the output.
 
 
 //your code...
 
+let loopCount = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15];
 
+for (let i = 0; i < loopCount.length; i++) {
+    console.log(loopCount[i])
+}
 
-
+/*let conversionFC = function(currentTempf) {
+    console.log("conversion function")
+         var celsius = (currentTempf - 32) * 5 / 9;
+        console.log(celsius);   
+    }
+    
+    conversionFC(currentTempf)*/
 /************************************************************* */
 // Problem 7:
-// Write a JavaScript function that iterates the integers from 1 to 100. For multiples of 3 print "TEK" instead of the number and for multiples of 5, print "camp." For numbers that are multiples of both 3 and 5, print "TEKcamp."
+// Write a JavaScript function that iterates the integers from 1 to 100. 
+//For multiples of 3 print "TEK" instead of the number and for multiples 
+//of 5, print "camp." For numbers that are multiples of both 3 and 5, print "TEKcamp."
 
 //your code...
 
